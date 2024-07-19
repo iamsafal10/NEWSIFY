5f76d6302292fe08be5057d69675819c6a0a31c8
iamsafal10 <i.am.safaltriapthi@gmail.com>
2024-07-20 00:56:07 +0530

    image credits added, dark mode feature added

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import {theme} from "./context"
 
 export default function Page2() {
   // to view or hide sidebar
@@ -13,46 +14,52 @@ export default function Page2() {
   const toggledark=()=>{
     setdark(!dark);
   }
+
+
   return (
+    <>
+    <theme.Provider value={{dark,setdark}}>
     <div className="p2 flex">
-      {showSidebar == true && <Sidebar show={show} dark={dark}/>}
+      {showSidebar == true && <Sidebar show={show} />}
       {showSidebar == false && (
-        <div className="left_p2 h-full w-full  border-2  border-red-700">
+        <div className={`${dark?'dark':''} left_p2 h-full w-full  border-2  border-red-700`}>
           <button onClick={show}>
-            <img className="sidebar_icons" src="/menu.png" />
+            <img alt="credits:freepik.com" className={`${dark?'flip':''} sidebar_icons`} src="/menu.png" />
           </button>
         </div>
       )}
-      <div className="right_p2 w-full ">
-        <div className="right_p2_navbar w-full border-2 border-red-700">
+      <div className="right_p2  w-full ">
+        <div className={` ${dark?'dark':''} right_p2_navbar w-full border-2 border-red-700`}>
           <label className="search_bar left_navbar " for="search">
             <input
               type="text"
-              className="italic"
+              className={`${dark?'search_flip':''} italic`}
               placeholder=" Search here"
             ></input>
             <button>
-              <img className="search" src="/search_sidebar.svg" />
+              <img alt="credits:freepik.com" className={`${dark?'flip':''} search sidebar_icons`} src="/search_sidebar.svg" />
             </button>
             </label>
-            <div className={`right_navbar`}>
+            <div className={`${dark?'dark':''} right_navbar`}>
             <button onClick={toggledark}>
-              <img className="search sidebar_icons" src="/dark.png" />
+              <img alt="credits:freepik.com" className={`${dark?'flip':''} search sidebar_icons`} src="/dark.png" />
             </button>
             <button>
-              <img className="search sidebar_icons" src="/dots.png" />
+              <img alt="credits:freepik.com" className={`${dark?'flip':''} dots sidebar_icons`} src="/dots.png" />
             </button>
             <button>
-              <img className="search sidebar_icons" src="/favourites.svg" />
+              <img alt="credits:freepik.com" className={`${dark?'flip':''} favourites sidebar_icons`} src="/favourites.svg" />
             </button>
             </div>
             
           
         </div>
-        <div className={`right_p2_body border-2 border-blue-600`}>
-          <img className="default" src="default.jpg"></img>
+        <div className={`${dark?'dark':''} right_p2_body border-2 border-blue-600`}>
+          <img alt="credits:freepik.com" className={`${dark?'flip':''} default`} src="default.jpg"/>
         </div>
       </div>
     </div>
+    </theme.Provider>
+    </>
   );
 }
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import {theme} from "./context"
+
 
 export default function Sidebar(props)
 {
+    const value=React.useContext(theme)
     return(
-        <div className={`sidebar border-2 border-red-500 p-2 `}>
-            <button onClick={props.show}><img className="sidebar_icons" src="/menu.png"/></button>
+        <div className={` ${value.dark?'dark':''} sidebar border-2 border-red-500 p-2 `}>
+            <button onClick={props.show}><img className={`${value.dark?'flip':''}  sidebar_icons`} src="/menu.png"/></button>
         
         <ul className="ul">
-            <li className="sidebar-list flex gap-2"><img className="sidebar_icons"src="/home.svg"/>Home </li>
-            <li className="sidebar-list flex gap-2"><img className="sidebar_icons"src="/search_sidebar.svg"/>Search</li>
-            <li className="sidebar-list flex gap-2"><img className="sidebar_icons"src="/settings.svg"/>Settings</li>
-            <li className="sidebar-list flex gap-2"><img className="sidebar_icons"src="/favourites.svg"/>Favourites</li>
+            <li className="sidebar-list flex gap-2"><img className={`${value.dark?'flip':''} sidebar_icons`}src="/home.svg"/>Home </li>
+            <li className="sidebar-list flex gap-2"><img className={`${value.dark?'flip':''} sidebar_icons`}src="/search_sidebar.svg"/>Search</li>
+            <li className="sidebar-list flex gap-2"><img className={`${value.dark?'flip':''} sidebar_icons`}src="/settings.svg"/>Settings</li>
+            <li className="sidebar-list flex gap-2"><img className={`${value.dark?'flip':''} sidebar_icons`}src="/favourites.svg"/>Favourites</li>
         </ul>
         </div>
     )
